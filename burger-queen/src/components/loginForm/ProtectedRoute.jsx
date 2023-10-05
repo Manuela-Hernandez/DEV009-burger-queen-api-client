17894df714a729829e0fe74cd41d8984d71d281c
Manuela Hernadez <manu.hdz.0795@gmail.com>
2023-10-05 09:20:39 -0500

    Se agrega test de login

--- burger-queen/src/components/loginForm/ProtectedRoute.jsx
+++ burger-queen/src/components/loginForm/ProtectedRoute.jsx
@@ -1,20 +1,19 @@
 import { Navigate } from "react-router-dom"
 import PropTypes from "prop-types"
 
-export const ProtectedRoute = ({ children, requiredRole/*, redirecTo="/login"*/}) => {
-    const role = localStorage.getItem('role');
-    const name = localStorage.getItem('name');
-    const token = localStorage.getItem('token');
-    if(!role && !name && !token){
-        return <Navigate to={"/login"} />
-    } else if (role !== requiredRole ) {
-        return <Navigate to={"/denied"} />
-    }
-    return children
+export const ProtectedRoute = ({ children, requiredRole }) => {
+  const role = localStorage.getItem('role');
+  const name = localStorage.getItem('name');
+  const token = localStorage.getItem('token');
+  if (!role || !name || !token) {
+    return <Navigate to={"/login"} />
+  } else if (role !== requiredRole) {
+    return <Navigate to={"/denied"} />
+  }
+  return children
 }
 
 ProtectedRoute.propTypes = {
-    children: PropTypes.node.isRequired,
-    requiredRole: PropTypes.string.isRequired,
-    redirecTo: PropTypes.string
+  children: PropTypes.node.isRequired,
+  requiredRole: PropTypes.string.isRequired,
 }
--- burger-queen/src/routes/login/login.test.jsx
+++ burger-queen/src/routes/login/login.test.jsx
@@ -1,65 +1,60 @@
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
-import { describe, it } from "@jest/globals";
-import { useNavigate } from 'react-router-dom';
+import { render, fireEvent, waitFor } from "@testing-library/react";
 import Login from "./login.jsx";
-import { queryByTestId } from "@testing-library/dom";
-import { get } from "https";
-// Mock de axios o funcion de petición
+
+const mockedUseNavigate = jest.fn();
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'), // Mantiene las exportaciones originales
-  useNavigate: jest.fn({}), // Crea un mock de useNavigate
+  useNavigate: () => mockedUseNavigate, // Crea un mock de useNavigate que lo inspecciona
 }));
 
 describe("Login", () => {
-  //Agregar un beforeEach
-    it("should render Login", () => {
-      useNavigate.mockReturnValue(() => {});
-      render(<Login />);
-      screen.debug();
+  beforeEach(() => {
+    jest.clearAllMocks();
+    mockedUseNavigate.mockRestore();// limpia mock de useNavigate
+  });
+  it("should render Login", () => {
+    render(<Login />);
+  });
+
+  it("Debería mostrar mensaje de error cuando no se introducen datos en los campos de email y contraseña", async () => {
+    const { getByText } = render(<Login />);
+    const btnLogin = getByText('Login')
+    const messageError = document.querySelector('#message');
+    fireEvent.click(btnLogin);
+
+    await waitFor(() => {
+      expect(messageError.textContent).toBe('Email and password are required');
     });
-    it("Debería mostrar mensaje de error cuando no se introducen datos en los campos de email y contraseña", async() => {
-      const { getByPlaceholderText, getByText } = render(<Login />);
-      screen.debug();
-      const btnLogin = getByText('Login')
-      const messageError = document.querySelector('#message');
+  });
+  it("Debería navegar a /waiter al introducir las credenciales de un mesero", async () => {
+    const { getByPlaceholderText, getByText } = render(<Login />);
+    const email = getByPlaceholderText('Email');
+    const password = getByPlaceholderText('Password');
+    const btnLogin = getByText('Login')
+    
+    fireEvent.change(email, { target: { value: 'empleado1@systers.xyz' } });
+    fireEvent.change(password, { target: { value: '0123456789' } });
+    fireEvent.click(btnLogin);
 
-      fireEvent.click(btnLogin);
-
-      await waitFor(() => {
-        expect(messageError.textContent).toBe('Email and password are required');
-      });
-      screen.debug();
+    await waitFor(() => {
+      expect(mockedUseNavigate).toBeCalledTimes(1);
+      expect(mockedUseNavigate).toBeCalledWith('/waiter');
     });
-    it("Debería navegar a /waiter al introducir las credenciales de un mesero", async() => {
-      const { getByPlaceholderText, getByText } = render(<Login />);
-      // screen.debug();
-      const email = getByPlaceholderText('Email');
-      const password = getByPlaceholderText('Password');
-      const btnLogin = getByText('Login')
-      const messageError = document.querySelector('message');
-      fireEvent.change(email, { target: { value: 'empleado1@systers.xyz' } });
-      fireEvent.change(password, { target: { value: '0123456789' } });
-      fireEvent.click(btnLogin);
+  });
+  it("Debería navegar a /admin al introducir las credenciales de un administrador", async () => {
+    const { getByPlaceholderText, getByText } = render(<Login />);
 
-      await waitFor(() => {
-        expect(window.location.pathname).toBe('/waiter');
-      });
-      //screen.debug();
+    const email = getByPlaceholderText('Email');
+    const password = getByPlaceholderText('Password');
+    const btnLogin = getByText('Login')
+
+    fireEvent.change(email, { target: { value: 'grace.hopper@systers.xyz' } });
+    fireEvent.change(password, { target: { value: '123456' } });
+    fireEvent.click(btnLogin);
+
+    await waitFor(() => {
+      expect(mockedUseNavigate).toBeCalledTimes(1);
+      expect(mockedUseNavigate).toBeCalledWith('/admin');
     });
-    it("Debería navegar a /waiter al introducir las credenciales de un mesero", async() => {
-      const { getByPlaceholderText, getByText } = render(<Login />);
-
-      const email = getByPlaceholderText('Email');
-      const password = getByPlaceholderText('Password');
-      const btnLogin = getByText('Login')
-
-      fireEvent.change(email, { target: { value: 'grace.hopper@systers.xyz' } });
-      fireEvent.change(password, { target: { value: '123456' } });
-      fireEvent.click(btnLogin);
-
-      await waitFor(() => {
-        // Intentar con espías
-        expect(useNavigate).toHaveBeenCalledWith('/admin');
-      });
-    });
+  });
 });

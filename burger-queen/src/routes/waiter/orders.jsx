6092c5ca8980283565e1fa0ed830ac5cbfb7dd89
Manuela Hernadez <manu.hdz.0795@gmail.com>
2023-10-06 10:52:18 -0500

    Se agrega configuracion tailwind y se reemplaza estilos css por tailwind

--- burger-queen/src/routes/waiter/orders.jsx
+++ burger-queen/src/routes/waiter/orders.jsx
@@ -1,20 +1,26 @@
-import './orders.css';
 import { useNavigate } from 'react-router-dom';
+
 export default function Root() {
   const navigateTo = useNavigate();
-  function logOut () {
+  function logOut() {
     localStorage.clear();
     navigateTo('/login');
   }
-    return (
-      <>
-        <nav id='menu-bar'>
-          <img src="https://i.postimg.cc/Nf0qKLPK/logoBQ.png" alt="Logo de Burger Queen"/>
-          <p>{localStorage.name}</p>
-          <img src="https://i.postimg.cc/g0ZT2WP6/exit-icon.png" alt="Icono de salida" id='exit-icon' onClick={logOut}/>
-        </nav>
-      </>
-    );
+  return (
+    <>
+      <nav id="menu-bar"
+        className="grid grid-cols-3 grid-rows-1 bg-bgqueen-secondary items-center h-24">
+        <img src="https://i.postimg.cc/Nf0qKLPK/logoBQ.png"
+          alt="Logo de Burger Queen"
+          className="h-full" />
+        <p className="justify-self-center text-3xl text-bgqueen-primary text-bold">{localStorage.name}</p>
+        <img src="https://i.postimg.cc/g0ZT2WP6/exit-icon.png"
+          alt="Icono de salida" id="exit-icon"
+          className="h-1/2 justify-self-end"
+          onClick={logOut} />
+      </nav>
+    </>
+  );
 }
 /*<ul>
             <li>RESTAURANTE</li>

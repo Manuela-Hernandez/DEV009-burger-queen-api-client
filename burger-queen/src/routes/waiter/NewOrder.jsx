9f9f3be086fbf9ff3966ee19387d158ca237759e
Manuela Hernadez <manu.hdz.0795@gmail.com>
2023-10-11 16:09:50 -0500

    Se cambia estilos en resumen de pedido, se agrega total al estado

--- burger-queen/src/routes/waiter/NewOrder.jsx
+++ burger-queen/src/routes/waiter/NewOrder.jsx
@@ -5,7 +5,7 @@ import NavigationBar from '../../components/NavigationBar/NavigationBar';
 import ResumenPedidos from '../../components/ResumenPedidos/Resumen'
 export default function NewOrder() {
   const [selectedProductType, setSelectedProductType] = useState(null);
-  const [customerName, setCustomerName]= useState('');
+  const [customerName, setCustomerName] = useState('');
   const handleCustomerNameChange = (e) => {
     setCustomerName(e.target.value);
   };
@@ -14,17 +14,20 @@ export default function NewOrder() {
       <NavigationBar />
       <section className='grid grid-cols-2 grid-rows-3 m-8'>
         <article className='grid grid-cols-2 grid-rows-2'>
-          <input type="text" placeholder='Nombre del cliente' onChange={handleCustomerNameChange} className='h-12 col-span-2 m-2 rounded-md bg-gray-200 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-xl' />
-          <button className='bg-white m-2 p-2 rounded' onClick={()=> setSelectedProductType('Desayuno')}>Desayuno</button>
-          <button className='bg-white m-2 p-2 rounded' onClick={()=> setSelectedProductType('Almuerzo')}>Almuerzo</button>
+          <input type="text"
+            placeholder='Nombre del cliente'
+            onChange={handleCustomerNameChange}
+            className='h-12 col-span-2 m-2 rounded-md bg-gray-200 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-xl' />
+          <button className='bg-white m-2 p-2 rounded' onClick={() => setSelectedProductType('Desayuno')}>Desayuno</button>
+          <button className='bg-white m-2 p-2 rounded' onClick={() => setSelectedProductType('Almuerzo')}>Almuerzo</button>
         </article>
 
         <article className='row-span-4'>
-          <ResumenPedidos customerName={customerName}/>
+          <ResumenPedidos customerName={customerName} />
         </article>
 
         <article className='row-span-2 m-2'>
-          <AddProductToOrder  selectedProductType={selectedProductType} />
+          <AddProductToOrder selectedProductType={selectedProductType} />
         </article>
       </section>
       {/* <div className="grid grid-cols-2 grid-rows-1 mx-2 md:h-1/2 gap-4 ">

89b9c426b323f3b3f0b16ab5fc73596d09a85103
Manuela Hernadez <manu.hdz.0795@gmail.com>
2023-10-25 10:27:11 -0500

    Se agrega ruta anidada de todas las ordenes y se modifica tabla en componente chef

--- burger-queen/src/pages/waiter/NewOrder.jsx
+++ burger-queen/src/pages/waiter/NewOrder.jsx
@@ -1,6 +1,5 @@
-import { useState , useReducer} from 'react';
+import { useState, useReducer } from 'react';
 import AddProductToOrder from '../../components/AddProductsToOrder/AddProductsToOrder';
-import NavigationBar from '../../components/NavigationBar/NavigationBar';
 import OrderSummary from '../../components/OrderSummary/Summary'
 import { initialState, reducer } from './reducerActions';
 
@@ -18,25 +17,25 @@ export default function NewOrder() {
   };
   return (
     <>
-      <NavigationBar />
       <section className='grid grid-cols-2 grid-rows-3 m-8 border-2 border-bgqueen-secondary '>
         <article className='grid grid-cols-2 grid-rows-2'>
           <input type="text"
             placeholder='Customer name'
-            id = 'customerName'
+            id='customerName'
             onChange={handleCustomerNameChange}
             className='h-12 col-span-2 m-2 rounded-md bg-gray-200 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-xl' />
-            <button className={`m-2 p-2 rounded ${selectedProductType === 'Desayuno' ? 'bg-bgqueen-primary text-white' : 'bg-bgqueen-gray'}`} id = 'btn-breakfast' onClick={() => setSelectedProductType('Desayuno')}>Breakfast</button>
-            <button className={`m-2 p-2 rounded ${selectedProductType === 'Almuerzo' ? 'bg-bgqueen-primary text-white' : 'bg-bgqueen-gray'}`} id = 'btn-lunch' onClick={() => setSelectedProductType('Almuerzo')}>Lunch</button>
+          <button className={`m-2 p-2 rounded ${selectedProductType === 'Desayuno' ? 'bg-bgqueen-primary text-white' : 'bg-bgqueen-gray'}`} id='btn-breakfast' onClick={() => setSelectedProductType('Desayuno')}>Breakfast</button>
+          <button className={`m-2 p-2 rounded ${selectedProductType === 'Almuerzo' ? 'bg-bgqueen-primary text-white' : 'bg-bgqueen-gray'}`} id='btn-lunch' onClick={() => setSelectedProductType('Almuerzo')}>Lunch</button>
         </article>
 
         <article className='row-span-4'>
-          <OrderSummary customerName={customerName} order = {order} dispatch ={dispatch} clearInput={clearInput}/>
+          <OrderSummary customerName={customerName} order={order} dispatch={dispatch} clearInput={clearInput} />
         </article>
 
         <article className='row-span-2 m-2'>
-          <AddProductToOrder selectedProductType={selectedProductType} dispatch= {dispatch}/>
+          <AddProductToOrder selectedProductType={selectedProductType} dispatch={dispatch} />
         </article>
+    
       </section>
     </>
   );
